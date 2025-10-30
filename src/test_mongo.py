1526333ead45d014eb74ac482d460799db5ebb01
jouvence <spaceweb1997@gmail.com>
2025-10-30 10:18:10 +0100

    création de data_collection

--- src/test_mongo.py
+++ src/test_mongo.py
@@ -4,10 +4,10 @@ import os
 
 load_dotenv()
 
-client = MongoClient(os.getenv("MONGO_URL"))
+client = MongoClient(os.getenv("MONGO_URI"))
 
 try:
     client.admin.command('ping')
     print("Connexion à MongoDB réussie")
 except Exception as e:
-    print(f"Erreur de connexion à MongoDB: ", e)
+    print("Erreur de connexion à MongoDB: ", e)
