a973045923a68b174d744031ce0a1a7b21745145
jouvence <spaceweb1997@gmail.com>
2026-01-26 01:39:46 +0100

    nouveau notebook

--- data_collection/insertion_versmongo.py
+++ data_collection/insertion_versmongo.py
@@ -2,13 +2,25 @@ import pandas as pd
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+
+# Charger les variables d'environnement
 load_dotenv()
+
 client = MongoClient(os.getenv("MONGO_URI"))
-db = client[os.getenv("MONGO_DBNAME")]      
-collection = db["categorie"]          
-csv_path = r"..\data\category_tree.csv"
-df = pd.read_csv(csv_path)
-documents = df.to_dict(orient="records")
-result = collection.insert_many(documents)
-print(f" {len(result.inserted_ids)} documents insérés dans MongoDB.")
- 
+db = client[os.getenv("MONGO_DB_NAME")]
+
+# Liste de fichiers CSV à insérer
+csv_files = [
+    r"..\Data\row\item_properties_part1.csv",
+]
+
+# Insérer chaque CSV dans la collection
+for csv_path in csv_files:
+    print(f"Insertion du fichier : {csv_path}")
+    df = pd.read_csv(csv_path)
+    documents = df.to_dict(orient="records")
+    result = db["db_driven"].insert_many(documents)
+    print(f"{len(result.inserted_ids)} documents insérés depuis {csv_path}")
+        
+
+print("Tous les fichiers CSV ont été insérés dans MongoDB.")
